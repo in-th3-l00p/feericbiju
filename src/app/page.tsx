fdc7fc08ed94ff267412353812b86bb11efe73e0
Tisca Catalin <admin@tiscacatalin.com>
2025-08-09 22:45:51 +0300

    cta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import Collection from "./components/Collection";
+import Cta from "./components/Cta";
 
 export default function Home() {
   return (
     <main className="relative bg-white min-h-screen">
       <Hero />
       <Collection />
+      <Cta />
     </main>
   );
 }
